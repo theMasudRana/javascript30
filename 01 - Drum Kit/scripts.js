02c0c056fb5338afdb2438d088f7832f851feeb6
theMasudRana <mr.masudrana00@gmail.com>
2019-05-13 12:54:24 +0600

    Dram kit was awesome

---
+++ 01 - Drum Kit/scripts.js
@@ -0,0 +1,28 @@
+// Play sound when the key is pressed
+function playSound(e) {
+
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    if (!audio) return; // Return if there is no audio element
+    audio.currentTime = 0;
+    audio.play();
+
+    // Add class to key element
+    key.classList.add('playing');
+
+}
+
+
+// Remove transition from the key
+function removTransition(e) {
+    if (e.propertyName !== 'transform') return; // Return if it's not transition
+    this.classList.remove('playing');
+}
+
+// Look for the key is pressed
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removTransition));
+
+// Play the sound
+window.addEventListener('keydown', playSound);
